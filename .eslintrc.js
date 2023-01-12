4a6512657b3f72eb9174f73e71f84378aa4d3e0d
shunyue1320 <tsunyue1320@gmail.com>
2023-01-12 19:08:50 +0800

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-empty': ['error', { allowEmptyCatch: true }],
+  },
+}
