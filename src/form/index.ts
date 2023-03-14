807a20aa55d5ff1d5c52ebddce194a9063cc20ba
shunyue1320 <tsunyue1320@gmail.com>
2023-03-14 22:56:51 +0800

    chore: export FormArray

--- src/form/index.ts
+++ src/form/index.ts
@@ -3,9 +3,11 @@ import './index.less'
 import { Form } from './form'
 import { FormItem } from './form-item'
 import { attachPropertiesToComponent } from 'src/utils/attach-properties-to-component'
+import { FormArray } from './form-array'
 
 export type FormLayout = 'vertical' | 'horizontal'
 
 export default attachPropertiesToComponent(Form, {
   Item: FormItem,
+  Array: FormArray,
 })
